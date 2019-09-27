d18555d31c36c3feba1cba85f21902ce890ae230
jack seymour <stilljack@gmail.com>
2019-09-27 15:00:59 -0700

    init
    today we begin coding for me and my family, no longer to satisfy an external source i do not trust.
    
    it's sad to see lambda go from my life as I expected better things, but my instincts screamed at me "this house of cards is going to collapse"

---
+++ app/src/androidTest/java/com/saucefan/stuff/kierantration/ExampleInstrumentedTest.kt
@@ -0,0 +1,23 @@
+package com.saucefan.stuff.kierantration
+
+import androidx.test.platform.app.InstrumentationRegistry
+
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.saucefan.stuff.kierantration", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/saucefan/stuff/kierantration/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.saucefan.stuff.kierantration
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import androidx.core.view.get
+import androidx.core.view.size
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    makeGrid(5,5)
+
+
+
+    }
+
+
+
+
+    fun makeGrid(maxColumns: Int, maxRows: Int) {
+        gridView.columnCount =maxColumns
+        gridView.rowCount =maxRows
+        var itemsCount = gridView.columnCount * gridView.rowCount
+        if (itemsCount%2 ==0){
+            Toast.makeText(this,"is even",Toast.LENGTH_SHORT).show()
+        }
+        else{
+            gridView.columnCount++
+            itemsCount = maxColumns * maxRows
+            val new = gridView.rowCount * gridView.columnCount
+            Toast.makeText(this,"uh oh! we had to add a row, can you figure out why? Hint: we want you to be able to win!",Toast.LENGTH_LONG).show()
+        }
+        var row = 0
+        var column = 0
+        for (i in 0 until itemsCount) {
+            val view = ImageView(this)
+            //change colors
+            when (i % 7) {
+                0 -> view.setBackgroundColor(Color.RED)
+                1 -> view.setBackgroundColor(Color.GREEN)
+                2-> view.setBackgroundColor(Color.BLACK)
+                3 -> view.setBackgroundColor(Color.BLUE)
+                4->view.setBackgroundColor(Color.YELLOW)
+                5->view.setBackgroundColor(Color.GRAY)
+                6->view.setBackgroundColor(Color.CYAN)
+            }
+            val params = GridLayout.LayoutParams(GridLayout.spec(row, 1f), GridLayout.spec(column, 1f))
+            view.layoutParams =params
+            view.tag="$column/$row"
+            val newc=column
+            val newr = row
+
+            view.setOnClickListener {
+                checkTile(i,it.tag as String)
+                val dotSizeCurrent=gridView.size
+            }
+            gridView.addView(view)
+
+
+            column++
+
+            if (column >= maxColumns) {
+                column = 0
+                row++
+            }
+        }
+
+    }
+
+
+    //check tile will check the tile clicked,
+    fun checkTile(position:Int,tag:String){
+        Toast.makeText(this,"previous $position -- tag:$tag -- current ${gridView.size} c:${gridView.columnCount}  r:${gridView.rowCount}",Toast.LENGTH_SHORT).show()
+    }
+    inner class TileObj (val column:Int,val Row:Int){
+
+    }
+}
