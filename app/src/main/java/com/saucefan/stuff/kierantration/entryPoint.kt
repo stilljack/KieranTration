c66b7d4a7b39798c57edc073ede9558a78dd82ca
jack seymour <stilljack@gmail.com>
2019-09-27 17:40:43 -0700

    mom's birds

--- app/src/main/java/com/saucefan/stuff/kierantration/entryPoint.kt
+++ app/src/main/java/com/saucefan/stuff/kierantration/entryPoint.kt
@@ -45,7 +45,7 @@ class entryPoint : AppCompatActivity() {
                 editSanitization(s)
             }
         })
-        
+
         btn_enter.setOnClickListener{
             sanitizeEntries()
 
@@ -54,13 +54,14 @@ class entryPoint : AppCompatActivity() {
     }
 
 
-    // probably a more elegan way to do this but basically...
+    // probably a more elegant way to do this but basically...
     // checks if there's a char sequence at all,
-    // checks if the first character is a
+    // checks if the first character is a 0
     // if there's some digits beyond the zero to the right, it keeps them in the field
     // if zero would be the only number, it makes it a one instead
     fun editSanitization(s: CharSequence) {
-        if (s.isNotEmpty()) {
+
+        if (s.isNotBlank()) {
             if (s.substring(0, 1).contains("0") && s.length >= 2) {
                 if (et_rows.text.isNotEmpty()) {
                     et_rows.setText(et_rows.text.substring(1, et_rows.text.length))
@@ -73,23 +74,29 @@ class entryPoint : AppCompatActivity() {
     }
 
 
+    fun popText(string: String) {
 
-    fun popText(string:String){
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
+    }
 
-    Toast.makeText(this, string,Toast.LENGTH_SHORT).show()
-}
     fun sanitizeEntries() {
-        val rows = et_rows.text.toString().toInt()
-        val columns =et_columns.text.toString().toInt()
+        if (et_rows.text.isNotEmpty() && et_columns.text.isNotEmpty()) {
+            val rows = et_rows.text.toString()?.toInt()
+            val columns = et_columns.text.toString()?.toInt()
+            if (rows >= 1 || columns >= 1) {
+                val i = Intent(this, MainActivity::class.java)
+                i.putExtra("ourRows", rows)
+                i.putExtra("ourColumns", columns)
+                startActivity(i)
+            }
+        } else {
+            Toast.makeText(
+                this,
+                "Sorry we can't make a board without a real number of rows and columns! Try again!",
+                Toast.LENGTH_SHORT
+            ).show()
 
-        if (rows <= 0 || columns <=0){
-            Toast.makeText(this,"Sorry we can't make a board without a real number of rows and columns! Try again!",Toast.LENGTH_SHORT).show()
-        }
-        else {
-            val i = Intent(this, MainActivity::class.java)
-            i.putExtra("ourRows", rows)
-            i.putExtra("ourColumns", columns)
-            startActivity(i)
         }
     }
+
 }
